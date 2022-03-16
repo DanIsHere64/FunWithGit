3ee49da6f882bd3c365efc1f69564b71ef1d4339
DanIsHere64 <djt032@latech.edu>
2022-03-16 15:05:56 -0500

    Fun

--- CSC-132/Homework/2D_Points.py
+++ CSC-132/Homework/2D_Points.py
@@ -24,22 +24,27 @@ class Point:
 
 ##########################################################
 # ***DO NOT MODIFY OR REMOVE ANYTHING BELOW THIS POINT!***
+
 # create some points
 p1 = Point()
 p2 = Point(3, 0)
 p3 = Point(3, 4)
+
 # display them
 print("p1:", p1)
 print("p2:", p2)
 print("p3:", p3)
+
 # calculate and display some distances
 print("distance from p1 to p2:", p1.dist(p2))
 print("distance from p2 to p3:", p2.dist(p3))
 print("distance from p1 to p3:", p1.dist(p3))
+
 # calculate and display some midpoints
 print("midpt of p1 and p2:", p1.midpt(p2))
 print("midpt of p2 and p3:", p2.midpt(p3))
 print("midpt of p1 and p3:", p1.midpt(p3))
+
 # just a few more things...
 p4 = p1.midpt(p3)
 print("p4:", p4)
